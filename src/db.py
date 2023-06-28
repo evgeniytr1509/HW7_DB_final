9f7a94a9df6f6132581c2ab7a7795cff596becd8
evgeniytr1509 <95618994+evgeniytr1509@users.noreply.github.com>
2023-06-28 23:16:32 +0300

    Add files via upload

---
+++ src/db.py
@@ -0,0 +1,25 @@
+import configparser
+import pathlib
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+file_config = pathlib.Path(__file__).parent.parent.joinpath('config.ini')
+config = configparser.ConfigParser()
+config.read(file_config)
+
+username = config.get('DB', 'user')
+password = config.get('DB', 'password')
+db_name = config.get('DB', 'db_name')
+domain = config.get('DB', 'domain')
+port = config.get("DB", "port")
+
+url = f'postgresql://{username}:{password}@{domain}:{port}/{db_name}'
+
+engine = create_engine(url, echo=False, pool_size=5, max_overflow=0)
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+print(config)
